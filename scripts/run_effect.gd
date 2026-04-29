a1358070c25eca33e40e90e77c8d83466303ea78
Gonzalo <gonzalogallardo216@gmail.com>
2026-04-28 21:28:17 -0300

    video 15 a 18 minuto 40

--- scripts/run_effect.gd
+++ scripts/run_effect.gd
@@ -7,8 +7,13 @@ extends Node
 
 func _ready() -> void:
 	player.run_effect.connect(_on_player_run_effect)
-	
+	timer_run_effect.timeout.connect(_on_timeout)
 	
 func _on_player_run_effect() -> void:
-	player.must_run = true
-	timer_run_effect.start(duration_effect)
+	if timer_run_effect.time_left <= 0:
+		player.must_run = true
+	
+	timer_run_effect.start(duration_effect + timer_run_effect.time_left)
+
+func _on_timeout() -> void:
+	player.must_run = false
